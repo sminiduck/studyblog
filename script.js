6b0c278ade49e6b06b2efb71a47098140029b24e
Jeong Seung Min <98789188+sminiduck@users.noreply.github.com>
2025-04-07 10:42:50 +0000

    cisc vs risc 등 4개 메모 추가

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+
 // 하이라이트 전체 적용
 hljs.highlightAll();
 
 // MathJax 렌더링 요청 (전체 문서)
 MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "default", // 또는 'dark', 'forest', 'neutral'
+});
