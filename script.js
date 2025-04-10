3cc54d2f6d218b1f89d25f9964831e845f112231
Jeong Seung Min <98789188+sminiduck@users.noreply.github.com>
2025-04-10 13:57:59 +0000

    masonry grid 적용

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
 
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector('.memo-grid');
+  if (grid && window.Masonry) {
+    new window.Masonry(grid, {
+      itemSelector: '.container',
+      columnWidth: 520,
+      gutter: 20,
+      fitWidth: false
+    });
+  } else {
+    console.warn("Masonry.js or .memo-grid not loaded.");
+  }
+});
+
 // 하이라이트 전체 적용
 hljs.highlightAll();
 
-// MathJax 렌더링 요청 (전체 문서)
-MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+// // MathJax 렌더링 요청 (전체 문서)
+// MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 
 mermaid.initialize({
   startOnLoad: true,
   theme: "default", // 또는 'dark', 'forest', 'neutral'
 });
+
+MathJax.Hub.Queue(["Typeset", MathJax.Hub, () => {
+  if (window.msnry) window.msnry.layout();
+}]);
+
